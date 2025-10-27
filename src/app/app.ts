2fa889d708f8fad48225080d20d5ff72e494b840
SPYCIIII <zenatabdou06@gmail.com>
2025-10-27 20:04:51 +0100

    dashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Homescreen } from "./homescreen/homescreen";
 import { Optionlist } from './optionlist/optionlist';
+import { Dashboard } from "./dashboard/dashboard";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Homescreen, Optionlist],
+  imports: [RouterOutlet, Homescreen, Optionlist, Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
