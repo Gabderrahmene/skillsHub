227a71ae383fe03328858c895e0a1f28f5bb8900
SPYCIIII <zenatabdou06@gmail.com>
2025-11-01 20:26:24 +0100

    zdt forms f login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-
+loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+});
 }
