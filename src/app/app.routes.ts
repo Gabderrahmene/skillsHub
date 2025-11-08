128041cf03a32d328ee3024733f5cb1e28c9094d
SPYCIIII <zenatabdou06@gmail.com>
2025-11-08 18:34:27 +0100

    support page :/

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { Homescreen } from './components/homescreen/homescreen';
 import { Login } from './components/login/login';
 import { Register } from './components/register/register';
 import { StudentLayout } from './components/student-layout/student-layout';
+import { Module } from './components/module/module';
+import { Affichage } from './components/affichage/affichage';
+import { Contact } from './components/contact/contact';
 
 export const routes: Routes = [
     { path: "login", component: Login },
@@ -14,7 +17,10 @@ export const routes: Routes = [
         children: [
             { path: '', redirectTo: 'homescreen', pathMatch: 'full' },
             { path: 'homescreen', component: Homescreen },
-            { path: 'module', component: AllModulesPage }
+            { path: 'module', component: AllModulesPage },
+            { path: 'modulepage', component: Module },
+            { path: 'affichage', component: Affichage },
+            { path: 'contact', component: Contact },
         ]
     },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
